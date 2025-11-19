7fb592b4f8df0328291c81703e5f561b0fe76faf
Haruyoshi Gonsho <robi10@outlook.jp>
2025-11-20 07:24:47 +0900

    バリデーションエラー内容修正

--- src/main/java/com/todo/app/form/RegisterForm.java
+++ src/main/java/com/todo/app/form/RegisterForm.java
@@ -9,7 +9,7 @@ import lombok.Data;
 @Data
 public class RegisterForm {
 	@NotEmpty
-	@Size(max = 40, message = "{0}は{1}文字以上で入力してください。")
+	@Size(max = 40, message = "{0}は{1}文字以下で入力してください。")
 	@Email(message = "{0}はメールアドレスの形式で入力してください。")
 	private String userId;
 	
@@ -17,11 +17,11 @@ public class RegisterForm {
 	@Size(max = 40, message = "{0}は{1}文字以上で入力してください。")
 	private String userName;
 	
-	@Size(min = 5, max = 40, message = "{0}は{1}文字以上{2}文字以下で入力してください。")
+	@Size(max = 40, min = 5, message = "{0}は{1}文字以上{2}文字以下で入力してください。")
 	private String password;
 	
 	private Integer teamId;
 	
-	@Size(min = 5, max = 40, message = "{0}は{1}文字以上{2}文字以下で入力してください。")
+	@Size(max = 40, min = 5, message = "{0}は{1}文字以上{2}文字以下で入力してください。")
 	private String checkPassword;
 }
